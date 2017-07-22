602e59b0b76b1a34428a36acf4a9eea914e3ef74
hillerlin <670758473@qq.com>
2017-07-22 19:03:16 +0800

    update config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,10 +5,10 @@ return array(
     'TMPL_ACTION_SUCCESS'   =>  APP_PATH.'Admin/message.html', // 默认成功跳转对应的模板文件
     //数据库配置信息
     'DB_TYPE'                       => 'mysql', // 数据库类型
-    'DB_HOST'                       => '127.0.0.1', // 服务器地址
+    'DB_HOST'                       => '59.110.136.193', // 服务器地址
     'DB_NAME'                       => 'gt_riskdata', // 数据库名
     'DB_USER'                       => 'root', // 用户名
-    'DB_PWD'                        => '', // 密码
+    'DB_PWD'                        => 'lmj4282887', // 密码
     'DB_PORT'                       =>  3306, // 端口
     'DB_CHARSET'                    =>  'utf8', // 字符集
     'DATA_CACHE_TIME'               =>  600,
